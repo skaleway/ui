ffd77422483cdc4e897c532ca59be8621790a4d0
bossadizenith <org.codeen@gmail.com>
2026-01-04 19:30:05 +0100

    feat: setup the basic landing page from the nextjs boiler plate

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,46 @@
+import { Icons } from "@/components/icons";
+import { siteConfig } from "@/lib/site";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="text-4xl font-bold flex items-end border icon">
-          <span>UI</span>
-          <div className="size-0.5 rounded-full bg-orange-600" />
-        </div>
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="flex min-h-screen items-center justify-center bg-muted font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col  justify-between py-32 px-8 md:px-16 bg-background sm:items-start border-x">
+        <Icons.logoLight />
+
+        <div className="flex flex-col items-center gap-6 text-start">
+          <h1 className="max-w-xl md:text-3xl text-2xl font-mono font-semibold leading-10 tracking-tight">
+            Beautiful, fast, and responsive UI Templates.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+          <p className="max-w-xl md:text-lg text-base leading-8 text-muted-foreground">
+            Looking for a starting point for your next project? Browse our
+            collection of open-source{" "}
+            <Link
+              href="/templates"
+              className="font-medium text-foreground font-mono"
             >
               Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+            </Link>{" "}
+            or let us build it for you at{" "}
+            <Link
+              href={siteConfig.author.url}
+              target="_blank"
+              className="font-medium text-foreground font-mono"
             >
-              Learning
-            </a>{" "}
-            center.
+              {siteConfig.author.name}
+            </Link>
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row text-muted-foreground">
+          Let us help you build your next project{" "}
+          <Link
+            href={siteConfig.author.url}
             target="_blank"
-            rel="noopener noreferrer"
+            className="font-medium text-foreground font-mono"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            @{siteConfig.author.name}
+          </Link>
         </div>
       </main>
     </div>
---
+++ lib/site.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  name: "UI.",
+  url: "https://ui.com",
+  ogImage: "https://ui.com/og.png",
+  description: "Beautiful, fast, and responsive UI Templates.",
+  links: {
+    twitter: "https://twitter.com/ui",
+    github: "https://github.com/ui",
+  },
+  author: {
+    name: "skaleway",
+    url: "https://skaleway.com",
+    email: "contact@skaleway.com",
+  },
+};
