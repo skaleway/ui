057f596dfa4141a9f2b251778c51c18d23b8b720
bossadizenith <org.codeen@gmail.com>
2026-01-16 23:57:44 +0100

    feat: done with the navbar'

--- app/(templates)/templates/e-commerce/_components/header.tsx
+++ app/(templates)/templates/e-commerce/_components/header.tsx
@@ -1,12 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { Notification01Icon } from "@hugeicons/core-free-icons";
+import { Notification01Icon, Search01Icon } from "@hugeicons/core-free-icons";
 import React from "react";
+import { Input } from "@/components/ui/input";
+import { Kbd, KbdGroup } from "@/components/ui/kbd";
 
 export const CommerceHeader = () => {
   return (
     <header className="flex items-center justify-between h-14 border-b px-6">
-      <div>CommerceHeader</div>
+      <div className="h-9 max-w-sm w-full relative">
+        <HugeiconsIcon
+          icon={Search01Icon}
+          className="absolute left-2 top-1/2 -translate-y-1/2 size-4 text-muted-foreground"
+        />
+        <Input placeholder="Search" className="px-8" />
+        <KbdGroup className="absolute right-2 top-1/2 -translate-y-1/2">
+          <Kbd>Ctrl</Kbd>
+          <span className="text-muted-foreground">+</span>
+          <Kbd>K</Kbd>
+        </KbdGroup>
+      </div>
       <Button variant="outline" size="icon" className="relative">
         <HugeiconsIcon icon={Notification01Icon} />
         <span className="absolute right-1 top-1 size-2 rounded-full bg-destructive" />
