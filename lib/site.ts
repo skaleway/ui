c36f3ee0d2cfff7675bc0445a3029e88a83e6df5
bossadizenith <org.codeen@gmail.com>
2026-01-17 14:38:39 +0100

    feat: added some og things

--- lib/site.ts
+++ lib/site.ts
@@ -1,11 +1,11 @@
 export const siteConfig = {
   name: "UI.",
-  url: "https://ui.com",
-  ogImage: "https://ui.com/og.png",
-  description: "Beautiful, fast, and responsive UI Templates.",
+  url: "https://ui.skaleway.com",
+  ogImage: "https://ui.skaleway.com/og.png",
+  description: "Open source UI templates for Next.js built with shadcn/ui.",
   links: {
-    twitter: "https://twitter.com/ui",
-    github: "https://github.com/ui",
+    twitter: "https://twitter.com/skaleway",
+    github: "https://github.com/skaleway",
   },
   author: {
     name: "skaleway",
