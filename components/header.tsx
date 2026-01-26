86a4cf258fcb8157c5225c463835ec3ed022ea8f
bossadizenith <org.codeen@gmail.com>
2026-01-26 23:57:51 +0100

    feat: nothing

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,8 @@ import { Icons } from "./icons";
 import { ModeToggle } from "./mode-toggle";
 import Link from "next/link";
 
+// nothing
+
 export const Header = () => {
   return (
     <header className="flex items-center justify-between w-full">
